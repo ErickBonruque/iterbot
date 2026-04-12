73bd605e87d3254c2f67e8d8c861709d2c13f5d2
Erick Bonruque <bonruqueruck@gmail.com>
2026-04-12 14:53:47 -0300

    test(jobs): criar testes unitários para Company, Job e JobApplication
    Task: 01-01-12
    - TestCompany: criação e validação de CNPJ
    - TestJob: criação e relacionamento com Company
    - TestJobApplication: criação e unique constraint
    - Usa pytest-django e factories

---
+++ apps/jobs/tests/test_models.py
@@ -0,0 +1,49 @@
+import pytest
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from apps.jobs.tests.factories import CompanyFactory, JobFactory, JobApplicationFactory
+from apps.jobs.models import CompanyStatus, JobStatus
+
+
+@pytest.mark.django_db
+class TestCompany:
+    def test_company_creation(self):
+        """Test Company model creation"""
+        company = CompanyFactory()
+        assert company.pk is not None
+        assert company.status == CompanyStatus.APPROVED
+    
+    def test_company_cnpj_validation(self):
+        """Test CNPJ validation"""
+        company = CompanyFactory.build(cnpj="00.000.000/0000-00")
+        with pytest.raises(ValidationError):
+            company.full_clean()
+
+
+@pytest.mark.django_db
+class TestJob:
+    def test_job_creation(self):
+        """Test Job model creation"""
+        job = JobFactory()
+        assert job.pk is not None
+        assert job.status == JobStatus.APPROVED
+    
+    def test_job_company_relationship(self):
+        """Test Job belongs to Company"""
+        job = JobFactory()
+        assert job.company is not None
+        assert job in job.company.jobs.all()
+
+
+@pytest.mark.django_db
+class TestJobApplication:
+    def test_job_application_creation(self):
+        """Test JobApplication creation"""
+        application = JobApplicationFactory()
+        assert application.pk is not None
+    
+    def test_job_application_unique_constraint(self):
+        """Test unique constraint (user, job)"""
+        application = JobApplicationFactory()
+        with pytest.raises(IntegrityError):
+            JobApplicationFactory(user=application.user, job=application.job)
